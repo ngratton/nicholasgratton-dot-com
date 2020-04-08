d6f163e98a584afe371035995d31c8ae68e5f527
Nicholas Gratton <nichgratton@gmail.com>
2020-04-08 15:27:19 -0400

    Prêt au déploiement

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
+/**
+ * Gestion du formulaire
+ */
+window.addEventListener('DOMContentLoaded', function() {
 
-
-// Talents
-var barres = document.querySelectorAll('.barre-mesure')
-
-barres.forEach(barre => {
-    var mesure = barre.getAttribute('id') 
-    var barreMesure = document.querySelector(`#${mesure}`)
-    var barreWidth = barreMesure.style.width
-    console.log(barre.style.width)
+    // get the form elements defined in your form HTML above
+    var form = document.querySelector('#form-contact')
+    var button = document.querySelector('#btn-form-submit')
+    var status = document.querySelector('#form-status')
+    var statusText = document.querySelector('#form-status span')
+    var statusDot = document.querySelector('#dot-status')
     
-    // setTimeout(() => {
-    //     console.log(barreMesure.style)
-    // }, 1000)
-})
-
-// var main = document.querySelector('main')
-// main.addEventListener('scroll', e => {
-//     console.log('Scrolling !')
+    // Success and Error functions for after the form is submitted    
+    function success() {
+        form.reset()
+        button.style = 'display: none'
+        status.style = 'display: flex'
+        status.classList.add('succes')
+        statusDot.classList.add('svg-succes')
+        statusText.innerHTML = 'Merci pour votre message !'
+    }
     
-// })
+    function error() {
+        status.classList.add('erreur')
+        status.sytle = 'display: flex'
+        statusDot.classList.add('svg-erreur')
+        statusText.innerHTML = 'Oups ! Il y a eu un problème.'
+    }
+
+    // handle the form submission event
+    form.addEventListener('submit', function(ev) {
+      ev.preventDefault()
+      var data = new FormData(form)
+      ajax(form.method, form.action, data, success, error)
+    })
+  })
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader('Accept', 'application/json')
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType)
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType)
+      }
+    }
+    xhr.send(data)
+  }
